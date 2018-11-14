9c1ab508e6431d5c0b0b9d708f076c4805a37594
Dhevan Lau <1433596@students.wits.ac.za>
2018-11-14 12:58:07 +0200

    Basic pause menu implemented

--- 3rd Year Game/Assets/Scripts/Dhevan's Scripts/GameManager.cs
+++ 3rd Year Game/Assets/Scripts/Dhevan's Scripts/GameManager.cs
@@ -5,16 +5,41 @@ using InControl;
 
 public class GameManager : MonoBehaviour {
 
+	private InputDevice controller;
+	public GameObject pausePanel;
 
-	#region
+	private bool gamePaused = false;
 
-	public static GameManager instance;
-
-	void Awake(){
-		instance = this;
+	void Start() 
+	{
+		gamePaused = false;
+		controller = InputManager.ActiveDevice;
+		pausePanel.SetActive (false);
 	}
 
-	#endregion
+	void Update()
+	{
+		if (controller.Action2.WasPressed) {
+			if (gamePaused == false) {
+				gamePaused = true;
+				//show pause menu and pause game
+				pausePanel.SetActive(true);
+				Time.timeScale = 0f;
 
-	public GameObject player;
+			} else {
+				//Unpause game and hide pause menu.
+				gamePaused = false;
+				pausePanel.SetActive (false);
+				Time.timeScale = 1;
+			}
+		}
+	}
+
+	void pauseGame(){
+
+	}
+
+	void unpauseGame(){
+
+	}
 }
